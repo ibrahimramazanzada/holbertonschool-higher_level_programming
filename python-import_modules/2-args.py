863423babcd4b4fd56f4edb1edf71a6b1f975720
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-03 16:58:18 +0000

    hm

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-str = input()
-argv = str.split()
-if len(argv) == 1:
-    print("1 argument:")
-else:
-    print(f"{len(argv)} arguments:")
-for i in range(len(argv)):
-    print(f"{i + 1}: {argv[i]}")
+if __name__ == "__main__":
+    str = input()
+    argv = str.split()
+    if len(argv) == 1:
+        print("1 argument:")
+    else:
+        print(f"{len(argv)} arguments:")
+    for i in range(len(argv)):
+        print(f"{i + 1}: {argv[i]}")
