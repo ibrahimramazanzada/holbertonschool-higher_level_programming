1be14ea4fd4dbc764d823d3b0731afdb662f26f1
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-04 16:08:10 +0000

    data

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     if my_list:
-            print("\n".join(["{:d}".format(x) for x in my_list]))
+        print("\n".join(["{:d}".format(x) for x in my_list]))
