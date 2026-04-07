ef103952162aedf5172da3148ee5ed4f11910b53
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-07 13:56:21 +0000

    class

---
+++ python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""""This module defines a Square class with a private instance attribute size."""
+
+class Square:
+    """Class Square that defines a square with a private instance attribute size."""
+    def __init__(self, size=0):
+        """Initialize the Square instance with a private instance attribute size."""
+        if not isinstance(size, (int)):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size ** 2
+    def size(self):
+        """Get the value of the private instance attribute size."""
+        return self.__size
+    def size(self, value):
+        """Set the value of the private instance attribute size."""
+        if not isinstance(value, (int)):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
