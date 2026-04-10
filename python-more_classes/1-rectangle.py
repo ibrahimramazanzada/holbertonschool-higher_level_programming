a876ae713100653a05361ef39fcd07400cec2745
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-10 12:27:14 +0000

    rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""This is a simple module that defines a Rectangle class"""
 
 
 class Rectangle:
+    """Represents a rectangle shape."""
     @property
     def width(self):
+        """The width of the rectangle."""
         return self.__width
     @width.setter
     def width(self, value):
+        """Set the width of the rectangle."""
         if value < 0:
             raise ValueError("width must be >= 0")
         if not isinstance(value, (int)):
@@ -14,11 +18,14 @@ class Rectangle:
         self.__width = value
     @property
     def height(self):
+        """The height of the rectangle."""
         return self.__height
     @height.setter
     def height(self, value):
+        """Set the height of the rectangle."""
         if value < 0:
             raise ValueError("height must be >= 0")
         if not isinstance(value, (int)):
             raise TypeError("height must be an integer")
         self.__height = value
+
