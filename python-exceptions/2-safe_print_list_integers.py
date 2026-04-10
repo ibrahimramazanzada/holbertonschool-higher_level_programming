6044a47f9633209712a43f47d285bc4d4c9a38df
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-10 14:38:41 +0000

    exc

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""Print integers from a list safely."""
+
+
 def safe_print_list_integers(my_list=[], x=0):
+    """Print the first x elements and return the count of integers."""
     count = 0
-    try:
-        for i in range(x):
-            element = my_list[i]
-            if isinstance(element, int) and not isinstance(element, bool):
-                print("{:d}".format(element), end="")
-                count += 1
-        print()
-    except (IndexError, ValueError):
-        print()
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except TypeError:
+            continue
+    print()
+
     return count
