b35053212c690e97909558f501c642937628f019
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-11 17:32:45 +0000

    try

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_matrix = []
-    for i in range(len(matrix)):
-        new_matrix.append(list(map(lambda x: x ** 2, matrix[i])))
-    return new_matrix
-
+    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
