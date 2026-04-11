ffac970e80d778682777e0cb2e15584d144cb4ce
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-11 17:26:04 +0000

    try

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
 
-    total_weight = sum(weight for weight, _ in my_list)
+    total_weight = sum(weight for _, weight in my_list)
     if total_weight == 0:
         return 0
 
