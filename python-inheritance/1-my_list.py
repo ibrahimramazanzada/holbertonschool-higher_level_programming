641f2d646462eee701004891ecf9d7ef5b21c874
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-13 13:44:22 +0000

    base

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is a simple module to do smth"""
+
+
+class MyList(list):
+    """This is a simple class to do smth"""
+
+    def print_sorted(self):
+        """This is a simple method to do smth"""
+        print(sorted(self))
