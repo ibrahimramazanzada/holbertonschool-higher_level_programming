7cdba76513af7a15ee34b6792cffeae55f90d605
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-13 14:09:44 +0000

    base

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is a simple module to do smth"""
+
+
+def is_kind_of_class(obj, a_class):
+    """This is a function that does smth."""
+    return isinstance(obj, a_class)
