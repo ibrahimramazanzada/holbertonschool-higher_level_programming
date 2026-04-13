e21354bde8549348525223a71fbd27099f9fb1e3
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-13 18:05:39 +0000

    abs

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,55 @@
+""""This module really does smth"""
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    """Abstract class for shapes"""
+
+    @abstractmethod
+    def area(self):
+        """Calculate area of shape"""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """Calculate perimeter of shape"""
+        pass
+
+
+class Circle(Shape):
+    """Circle shape"""
+
+    def __init__(self, radius):
+        """Initialize circle with radius"""
+        self.radius = radius
+
+    def area(self):
+        """Calculate area of circle"""
+        return pi * self.radius ** 2
+
+    def perimeter(self):
+        """Calculate perimeter of circle"""
+        return 2 * pi * self.radius
+
+
+class Rectangle(Shape):
+    """Rectangle shape"""
+
+    def __init__(self, width, height):
+        """Initialize rectangle with width and height"""
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """Calculate area of rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calculate perimeter of rectangle"""
+        return 2 * (self.width + self.height)
+
+def shape_info(shape):
+    """Print area and perimeter of shape"""
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
