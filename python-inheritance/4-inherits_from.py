8525643204f8762a20f9282630268fdcbc99a729
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-13 14:15:21 +0000

    base

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """This is a function that does smth."""
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
