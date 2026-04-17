e51c8148f2dd11c3c04c41cda9af1bffb35ddbbc
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-04-17 16:44:13 +0000

    adv

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module really does smth"""
+
+def add_attribute(obj, name, value):
+    """Adds an attribute to an object"""
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
