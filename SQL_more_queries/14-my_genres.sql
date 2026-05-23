597e72a03062ad30bfb477599bdb1a5ffc33d331
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-23 23:18:46 +0400

    sql

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,11 +1,7 @@
 -- list genres of a specific show
-SELECT tv_genres.name AS name WHERE tv_genres.id IN (
-    SELECT genre_id
-    FROM tv_show_genres
-    WHERE show_id = (
-        SELECT id
-        FROM tv_shows
-        WHERE title = 'Dexter'
-    )
-);
-ORDER BY name ASC;
+SELECT tv_genres.name 
+FROM tv_genres 
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id 
+WHERE tv_shows.title = 'Dexter' 
+ORDER BY tv_genres.name ASC;
