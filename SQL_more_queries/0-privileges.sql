09784405cae626b0f29fd57ea8d3a00f9e9adbe8
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-23 22:02:55 +0400

    sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- list user privileges of user_0d_1 and user_0d_2
+-- list user privileges
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,2 @@
+-- create user
+CREATE USER 'user_0d_1'@'localhost'
