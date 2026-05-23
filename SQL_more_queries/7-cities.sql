e3fec59c447f538e5e0c66c00372726c6ca19ba6
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-23 22:23:52 +0400

    sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,10 @@
 -- table with foreign key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL UNIQUE AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
