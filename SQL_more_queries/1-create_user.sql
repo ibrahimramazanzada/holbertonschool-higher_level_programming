61041ff761da63c931c88fd3d5465d293c36e9fb
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-23 22:05:48 +0400

    sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
--- create user
-CREATE USER 'user_0d_1'@'localhost'
+-- create user with password with all grants
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
