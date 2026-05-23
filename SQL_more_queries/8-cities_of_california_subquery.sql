15adbd75671c7ee17519ffe91fdc66c656e4ea8a
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-23 22:43:46 +0400

    sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- list all cities of California
-SELECT cities.name
+SELECT cities.id, cities.name
 FROM cities WHERE cities.state_id = (
     SELECT states.id
     FROM states
     WHERE states.name = 'California'
-);
+)
 ORDER BY cities.id ASC;
