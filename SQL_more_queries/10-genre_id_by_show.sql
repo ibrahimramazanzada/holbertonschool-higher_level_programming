6676f47be75f9eeb73487f1d8cf1535e57965e39
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-23 22:49:59 +0400

    sql

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- list titles and genre ids of all shows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_show_genres.genre_id, tv_shows.title ASC;
