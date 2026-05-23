3608933ccd1b9c9c9fc581b0fd465bab77f2006d
ibrahimramazanzada <ibrahim.ramazanzada@gmail.com>
2026-05-23 22:32:16 +0400

    sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- table in a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
